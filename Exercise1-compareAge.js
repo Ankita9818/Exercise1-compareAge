6f979c65140caff8a42dff9e7773c6faab7c4cd5
Ankita9818 <ankita.dixit@vinsol.com>
2017-09-27 18:17:10 +0530

    Exercise1 - Comparing Age of users commit

---
+++ Exercise1-compareAge.js
@@ -0,0 +1,23 @@
+//Constructor for User Object
+
+function User(name, age) {
+  this.name = name;
+  this.age = age;
+  //Compare function
+  User.prototype.compare = function(user) {
+    if (this.age < user.age){
+      return user.name + ' is older than ' + this.name;
+    }
+    else if (this.age == user.age) {
+      return this.name + ' and ' + user.name + ' are of same age';
+    }
+    else {
+      return this.name + ' is older than ' + user.name;
+    }
+  };
+}
+var user1 = new User('John', 18);
+var user2 = new User('Mary', 24);
+var result = user1.compare(user2);
+document.getElementById("demo").innerHTML = result;
+
